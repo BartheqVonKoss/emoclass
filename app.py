6d2009ac4267226df67de7ffca05feeedc01b09a
bartheq <bartlomiej@cross-compass.com>
2022-07-19 14:41:49 +0200

    added a few examples

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ get_docs()
 iface = gr.Interface(run_inference,
                      inputs=[gr.inputs.Textbox(lines=4),
                              gr.inputs.Dropdown(choices=["RandomForest", "LogisticRegression"])],
-                     outputs=[gr.outputs.Label(num_top_classes=5), "plot"])
-                     # examples=[[ "Enjoy the ride!" ]])#, "My favourite food is anything I didn't have to cook myself."])
+                     outputs=[gr.outputs.Label(num_top_classes=5), "plot"],
+                     examples=[["[NAME] is not fascist. Heck,she's not even alt-right", "LogisticRegression"],
+                               ["[NAME] is not fascist. Heck,she's not even alt-right", "RandomForest"],
+                               ["Shes awful and its all about her. Try again. And when the show has awful ratings thatll be confirmed.", "LogisticRegression"],
+                               ["Shes awful and its all about her. Try again. And when the show has awful ratings thatll be confirmed.", "RandomForest"],
+                               ["My favourite food is anything I didn't have to cook myself.", "LogisticRegression"],
+                               ["My favourite food is anything I didn't have to cook myself.", "RandomForest"],
+                               ["I’m really sorry about your situation :( Although I love the names Sapphira, Cirilla, and Scarlett!", "RandomForest"],
+                               ["I’m really sorry about your situation :( Although I love the names Sapphira, Cirilla, and Scarlett!", "LogisticRegression"]])
 iface.launch(server_name="0.0.0.0", server_port=1212)
